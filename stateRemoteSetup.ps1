43c617974e5c461110c72d13bd2d1f5571948753
Abhishek Sharma <abhishdevops@gmail.com>
2021-03-22 18:50:11 +0530

    Update stateRemoteSetup.ps1
    Minor clean up

--- stateRemoteSetup.ps1
+++ stateRemoteSetup.ps1
@@ -4,10 +4,10 @@ $STORAGE_ACCOUNT_NAME='*****'
 $CONTAINER_NAME='*****'
 
 # Create resource group
-#az group create --name $RESOURCE_GROUP_NAME --location eastus
+az group create --name $RESOURCE_GROUP_NAME --location eastus
 
 # Create storage account
-#az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
+az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
 
 # Get storage account key
 $ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query '[0].value' -o tsv)
